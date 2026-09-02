97afb8c9bbadb0c769c8d896d90b37f6bbe6781d
Marcelo Silva <marcelommx2@gmail.com>
2026-09-02 18:39:55 -0300

    feat: integra chatbot com Maritaca AI

--- api-ai/src/main/java/com/neabi/api_ai/ChatController.java
+++ api-ai/src/main/java/com/neabi/api_ai/ChatController.java
@@ -1,21 +1,25 @@
-import org.springframework.web.bind.annotation.RestController;
+package com.neabi.api_ai;
+
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class ChatController {
+
     private final ChatClient chatClient;
 
-    public ChatController(@notNull ChatClient.Builder chatClientBuilder) {
+    public ChatController(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
     }
 
-      @GetMapping("/ai")
-    String generation(String userInput) {
-        return this.chatClient.prompt()
-            .user(userInput)
-            .call()
-            .content();
+    @GetMapping("/ai")
+    public String generation(@RequestParam String userInput) {
+        return this.chatClient
+                .prompt()
+                .user(userInput)
+                .call()
+                .content();
     }
 }
